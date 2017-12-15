9a270fb29425ce99972e84f96c046a3decbf8183
repnz <repnzb@gmail.com>
2017-12-16 00:42:24 +0200

    Extract method ParseImportThunk

--- PE.Parser/ImportDirectoryParser.cpp
+++ PE.Parser/ImportDirectoryParser.cpp
@@ -10,12 +10,14 @@ private:
 	void LoadImportDescriptors(const uint32_t rva);
 public:
 	PEParser& _parser;
+	StreamParser& streamParser;
 	std::vector<ParsedImportDescriptor> _parsedDescriptors;
 	std::vector<ImportDescriptor> _descriptors;
 	const SectionHeader* _importSection;
 	explicit Impl(PEParser& parser);
 
 	void ParseImportDescriptors();
+	ImportedFunction ParseImportThunk(ImportThunk thunk);
 	bool Load();
 };
 
@@ -42,8 +44,25 @@ const std::vector<ParsedImportDescriptor>& ImportDirectoryParser::GetParsedImpor
 
 ImportDirectoryParser::~ImportDirectoryParser() = default;
 
-ImportDirectoryParser::Impl::Impl(PEParser& parser) : _parser(parser), _importSection(nullptr)
+ImportDirectoryParser::Impl::Impl(PEParser& parser) : _parser(parser), _importSection(nullptr), streamParser(parser.GetStreamParser())
 {
+}
+
+ImportedFunction ImportDirectoryParser::Impl::ParseImportThunk(ImportThunk thunk)
+{
+	const uint32_t val = thunk.ActualValue();
+
+	if (thunk.IsOrdinal())
+	{
+		// import by ordinal
+		return ImportedFunction(val);
+	}
+
+	// import by name
+	const uint32_t importByNamePtr = _importSection->GetFilePointer(val);
+	const uint16_t hint = streamParser.Read<uint16_t>(importByNamePtr);
+	const std::string functionName = streamParser.ReadString();
+	return ImportedFunction(functionName, hint);
 }
 
 void ImportDirectoryParser::Impl::ParseImportDescriptors()
@@ -69,27 +88,13 @@ void ImportDirectoryParser::Impl::ParseImportDescriptors()
 
 		for (size_t j=0; j<functionsSize; ++j)
 		{
-			const uint32_t val = thunks[j].ActualValue();
-
-			if (thunks[j].IsOrdinal())
-			{
-				parsedDescriptor.functions[j] = ImportedFunction(val);
-			}
-			else
-			{
-				const uint32_t importByNamePtr = _importSection->GetFilePointer(val);
-				const uint16_t hint = streamParser.Read<uint16_t>(importByNamePtr);
-				const std::string functionName = streamParser.ReadString();
-				parsedDescriptor.functions[j] = ImportedFunction(functionName, hint);
-			}
+			parsedDescriptor.functions[j] = ParseImportThunk(thunks[j]);
 		}
 	}
 }
 
 void ImportDirectoryParser::Impl::LoadImportDescriptors(const uint32_t startRva)
 {	
-	StreamParser& streamParser = _parser.GetStreamParser();
-
 	streamParser.Seek(_importSection->GetFilePointer(startRva));
 
 	const DataDirectory& importDir = _parser.GetDirectory(DirectoryOffsets::Import);
